80e6bc02d6c795382a375673a5ec13803be0fe94
Tristan Tarpley <tristan@learnwithjuni.com>
2020-10-11 17:01:50 -0500

    integrate typescript + eslint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const myEnv = dotenv.config();
+dotenvExpand(myEnv);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
