5972d7d2e2c3f42d387c4001e8d408e83f839283
Tristan Tarpley <tristan@learnwithjuni.com>
2020-10-11 23:07:22 -0500

    remove warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import './App.css';
 import { Login, SignUp } from 'app/userActions';
