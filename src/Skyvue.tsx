f163d608b16bf7c14ce592aa5843c47497f5463c
Tristan Tarpley <tristan@learnwithjuni.com>
2020-11-03 11:11:20 -0600

    added redirects to app. and live.

--- src/Skyvue.tsx
+++ src/Skyvue.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Login, SignUp } from 'app/userActions';
 import Home from 'app/home';
@@ -11,49 +11,59 @@ import ForgotPassword from 'app/userActions/ForgotPassword';
 import PasswordReset from 'app/userActions/PasswordReset';
 import DatasetWrapper from 'app/dataset/wrappers/DatasetWrapper';
 
-const Skyvue = () => (
-  <Router>
-    <Switch>
-      <Route path="/signup">
-        <PublicRoute>
-          <SignUp />
-        </PublicRoute>
-      </Route>
-      <Route path="/login">
-        <PublicRoute>
-          <Login />
-        </PublicRoute>
-      </Route>
-      <Route exact path="/forgot_password">
-        <PublicRoute>
-          <ForgotPassword />
-        </PublicRoute>
-      </Route>
-      <Route path="/forgot_password/:token">
-        <PublicRoute>
-          <PasswordReset />
-        </PublicRoute>
-      </Route>
-      <Route path="/logout">
-        <Logout />
-      </Route>
-      <Route path="/home">
-        <AuthenticatedRoute>
-          <App />
-        </AuthenticatedRoute>
-      </Route>
-      <Route path="/dataset/:datasetId">
-        <AuthenticatedRoute>
-          <DatasetWrapper />
-        </AuthenticatedRoute>
-      </Route>
-      <Route path="/">
-        <PublicRoute>
-          <Home />
-        </PublicRoute>
-      </Route>
-    </Switch>
-  </Router>
-);
+const Skyvue = () => {
+  useEffect(() => {
+    if (
+      process.env.NODE_ENV === 'production' &&
+      !window.location.host.includes('app.')
+    ) {
+      window.location.href = `https://app.skyvue.io${window.location.pathname}${window.location.search}`;
+    }
+  });
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signup">
+          <PublicRoute>
+            <SignUp />
+          </PublicRoute>
+        </Route>
+        <Route path="/login">
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        </Route>
+        <Route exact path="/forgot_password">
+          <PublicRoute>
+            <ForgotPassword />
+          </PublicRoute>
+        </Route>
+        <Route path="/forgot_password/:token">
+          <PublicRoute>
+            <PasswordReset />
+          </PublicRoute>
+        </Route>
+        <Route path="/logout">
+          <Logout />
+        </Route>
+        <Route path="/home">
+          <AuthenticatedRoute>
+            <App />
+          </AuthenticatedRoute>
+        </Route>
+        <Route path="/dataset/:datasetId">
+          <AuthenticatedRoute>
+            <DatasetWrapper />
+          </AuthenticatedRoute>
+        </Route>
+        <Route path="/">
+          <PublicRoute>
+            <Home />
+          </PublicRoute>
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default Skyvue;
--- src/app/home/HomePlaceholder.tsx
+++ src/app/home/HomePlaceholder.tsx
@@ -1,30 +1,26 @@
-import { Link } from 'react-router-dom';
 import React from 'react';
-import styled from 'styled-components/macro';
+import { Helmet } from 'react-helmet';
+// import styled from 'styled-components/macro';
 
-const HomeContainer = styled.div`
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+// const HomeContainer = styled.div`
+//   height: 100vh;
+//   width: 100%;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   flex-direction: column;
 
-  .actions__container {
-    a {
-      margin-right: 1rem;
-    }
-  }
-`;
+//   .actions__container {
+//     a {
+//       margin-right: 1rem;
+//     }
+//   }
+// `;
 
 const Home: React.FC = () => (
-  <HomeContainer>
-    <h2>SkyVue Data</h2>
-    <div className="actions__container">
-      <Link to="/login">Login</Link>
-      <Link to="/signup">Create account</Link>
-    </div>
-  </HomeContainer>
+  <Helmet>
+    <meta httpEquiv="refresh" content="0; url=https://live.skyvue.io" />
+  </Helmet>
 );
 
 export default Home;
