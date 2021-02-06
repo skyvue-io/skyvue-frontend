74678653a950113b3ff58cad50cc8ef481e81297
Tristan Tarpley <tristan@learnwithjuni.com>
2021-02-06 13:43:47 -0600

    temporarily remove redirect to app subdomain

--- src/Skyvue.tsx
+++ src/Skyvue.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import { Login, SignUp } from 'app/userActions';
@@ -11,59 +11,56 @@ import ForgotPassword from 'app/userActions/ForgotPassword';
 import PasswordReset from 'app/userActions/PasswordReset';
 import DatasetWrapper from 'app/dataset/wrappers/DatasetWrapper';
 
-const Skyvue = () => {
-  useEffect(() => {
-    if (
-      process.env.NODE_ENV === 'production' &&
-      !window.location.host.includes('app.')
-    ) {
-      window.location.href = `https://app.skyvue.io${window.location.pathname}${window.location.search}`;
-    }
-  });
-  return (
-    <Router>
-      <Switch>
-        <Route path="/signup">
-          <PublicRoute>
-            <SignUp />
-          </PublicRoute>
-        </Route>
-        <Route path="/login">
-          <PublicRoute>
-            <Login />
-          </PublicRoute>
-        </Route>
-        <Route exact path="/forgot_password">
-          <PublicRoute>
-            <ForgotPassword />
-          </PublicRoute>
-        </Route>
-        <Route path="/forgot_password/:token">
-          <PublicRoute>
-            <PasswordReset />
-          </PublicRoute>
-        </Route>
-        <Route path="/logout">
-          <Logout />
-        </Route>
-        <Route path="/home">
-          <AuthenticatedRoute>
-            <App />
-          </AuthenticatedRoute>
-        </Route>
-        <Route path="/dataset/:datasetId">
-          <AuthenticatedRoute>
-            <DatasetWrapper />
-          </AuthenticatedRoute>
-        </Route>
-        <Route path="/">
-          <PublicRoute>
-            <Redirect to="/login" />
-          </PublicRoute>
-        </Route>
-      </Switch>
-    </Router>
-  );
-};
-
+const Skyvue = () => (
+  // useEffect(() => {
+  //   if (
+  //     process.env.NODE_ENV === 'production' &&
+  //     !window.location.host.includes('app.')
+  //   ) {
+  //     window.location.href = `https://app.skyvue.io${window.location.pathname}${window.location.search}`;
+  //   }
+  // });
+  <Router>
+    <Switch>
+      <Route path="/signup">
+        <PublicRoute>
+          <SignUp />
+        </PublicRoute>
+      </Route>
+      <Route path="/login">
+        <PublicRoute>
+          <Login />
+        </PublicRoute>
+      </Route>
+      <Route exact path="/forgot_password">
+        <PublicRoute>
+          <ForgotPassword />
+        </PublicRoute>
+      </Route>
+      <Route path="/forgot_password/:token">
+        <PublicRoute>
+          <PasswordReset />
+        </PublicRoute>
+      </Route>
+      <Route path="/logout">
+        <Logout />
+      </Route>
+      <Route path="/home">
+        <AuthenticatedRoute>
+          <App />
+        </AuthenticatedRoute>
+      </Route>
+      <Route path="/dataset/:datasetId">
+        <AuthenticatedRoute>
+          <DatasetWrapper />
+        </AuthenticatedRoute>
+      </Route>
+      <Route path="/">
+        <PublicRoute>
+          <Redirect to="/login" />
+        </PublicRoute>
+      </Route>
+    </Switch>
+  </Router>
+);
 export default Skyvue;
